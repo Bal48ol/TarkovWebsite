fb89b530e8b6a58aad1bd806c1e49e5b733a4cfa
Bal48ol <day.of.travel69@gmail.com>
2023-09-03 18:13:15 +0300

    убран статус бар

--- app/src/main/java/com/example/tarkovwebsite/MainActivity.java
+++ app/src/main/java/com/example/tarkovwebsite/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.tarkovwebsite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,10 +13,12 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         webView = findViewById(R.id.webView);
 
